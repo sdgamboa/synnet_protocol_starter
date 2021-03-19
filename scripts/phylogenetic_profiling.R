06dbaa3f266df8090b6de45a754e0a9b6aa4c00b
Samuel Gamboa <sd.gamboa.t@gmail.com>
2021-03-19 17:11:56 -0600

    edited phylogenetic_profiling.R script

--- scripts/phylogenetic_profiling.R
+++ scripts/phylogenetic_profiling.R
@@ -1,22 +1,44 @@
 library(tidyverse)
+library(ggtree)
+
+my_tree <- read.tree(file = "../files/species_tree.newick")
 
 df <- read_tsv("TMBIM_clusters_nodes.csv") %>% 
-    arrange(family)
+    count(species, community) %>% 
+    pivot_wider(names_from = community, values_from = n)
 
-df <- df %>% 
-    count(species, family, community)
+sp_names <- gsub(" ", "_", df$species)
 
-p1 <- ggplot(data = df, 
-       mapping = aes(x = community, y = species, fill = n)) +
-    geom_tile() +
-    scale_fill_viridis_c() +
-    facet_wrap(~family, ncol = 1, scales = "free_y",
-               strip.position = "right") +
-    theme_classic() +
-    theme(axis.text.x = element_text(angle = 45, hjust = 1),
-          axis.text.y = element_text(face = "italic"))
+counts_data <- as.data.frame(df[,-1])
+rownames(counts_data) <- sp_names
 
-p1
 
-ggsave("Figure_6.pdf", p1,
-       width = 5, height = 6)
+p <- ggtree(my_tree, branch.length= "none") + 
+    geom_tiplab(as_ylab = TRUE) +
+    geom_cladelabel(node=MRCA(my_tree,
+                              c("Glycine_max","Medicago_truncatula")),
+                    label = "Fabaceae", fontsize = 4,
+                    align = TRUE, geom = "text",
+                    barsize = 0.5, angle = 90, hjust = 0.5, offset.text = .2) +
+    geom_cladelabel(node=MRCA(my_tree,
+                              c("Arabidopsis_thaliana","Brassica_oleracea")),
+                    label = "Brassicaceae", fontsize = 4,
+                    align = TRUE, geom = "text",
+                    barsize = 0.5, angle = 90, hjust = 0.5, offset.text = .2) +
+    geom_cladelabel(node=MRCA(my_tree,
+                              c("Sorghum_bicolor","Oryza_sativa")),
+                    label = "Poaceae", fontsize = 4,
+                    align = TRUE, geom = "text",
+                    barsize = 0.5, angle = 90, hjust = 0.5, offset.text = .2) +
+    vexpand(0.2, direction = -1) +
+    hexpand(0.2, direction = 1)
+
+
+pdf(file = "Figure_6.pdf")
+gheatmap(p, counts_data, offset=0.5, width=0.5, font.size=3, 
+         colnames_angle=45, hjust=1) + 
+    scale_fill_gradient(na.value = "gray85") +
+    theme(axis.text = element_text(size = 8,face = "bold"),
+          legend.position = "bottom") +
+    labs(fill = "# genes")
+dev.off()
